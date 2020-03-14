ff38e1c7f05834817e6a9e517923ebb12eeaaafe
ganjali <GAnjali@github.com>
2020-03-14 22:48:02 +0530

    Implement an Output for handling print statements

---
+++ src/main/java/IO/Output.java
@@ -0,0 +1,7 @@
+package IO;
+
+public class Output {
+    public void print(String word){
+        System.out.println(word);
+    }
+}
--- src/main/java/WordGeneratorMain.java
+++ src/main/java/WordGeneratorMain.java
@@ -1,4 +1,5 @@
 import IO.Input;
+import IO.Output;
 import models.Dictionary;
 import models.PhoneNumber;
 import models.WordGenerator;
@@ -12,14 +13,15 @@ import static helper.PhoneBookConstants.PHNUM_FILE_PATH;
 public class WordGeneratorMain {
     public static void main(String[] args) throws IOException {
         Input input = new Input();
+        Output output = new Output();
         Dictionary dictionary = new Dictionary(input.loadFile(DICT_FILE_PATH));
         WordGenerator wordGenerator = new WordGenerator(dictionary);
 
         List<String> phoneNumbers = input.loadFile(PHNUM_FILE_PATH);
-        for (String phoneNum : phoneNumbers){
+        for (String phoneNum : phoneNumbers) {
             PhoneNumber phoneNumber = new PhoneNumber(Integer.parseInt(phoneNum));
-            for(String word: wordGenerator.getDictionaryWords(phoneNumber)){
-                System.out.println(word);
+            for (String word : wordGenerator.getDictionaryWords(phoneNumber)) {
+                output.print(word);
             }
         }
     }
