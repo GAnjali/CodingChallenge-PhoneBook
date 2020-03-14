fff39acdce4b238a7bd3ff4dc5bcf5868f2c66e5
ganjali <GAnjali@github.com>
2020-03-14 08:13:17 +0530

    Implement getDigits of phone number

--- src/main/java/models/PhoneNumber.java
+++ src/main/java/models/PhoneNumber.java
@@ -1,13 +1,24 @@
 package models;
 
-public class PhoneNumber {
-    long phoneNumber;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public long getPhoneNumber() {
+public class PhoneNumber {
+    Integer phoneNumber;
+
+    public PhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(long phoneNumber) {
+    public void setPhoneNumber(Integer phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public List<Integer> getDigits() {
+        return String.valueOf(phoneNumber).chars().map(Character::getNumericValue).boxed().collect(Collectors.toList());
     }
 }
---
+++ src/test/java/model/PhoneNumberTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import models.PhoneNumber;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PhoneNumberTest {
+    private PhoneNumber phoneNumber;
+
+    @Before
+    public void init() {
+        phoneNumber = new PhoneNumber(2255);
+    }
+
+    @Test
+    public void shouldReturnListOfDigitsWhenCalledGetDigits() {
+        Assert.assertNotNull(phoneNumber.getDigits());
+        Assert.assertEquals(4, phoneNumber.getDigits().size());
+    }
+}
