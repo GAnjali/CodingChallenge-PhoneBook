070c688483f2f7471f5d8afdf53fd8604f2d6570
ganjali <GAnjali@github.com>
2020-03-20 19:05:25 +0530

    Implement PhoneBook.java for handling phoneNumbers and generate words and display using Output

--- src/main/java/IO/Output.java
+++ src/main/java/IO/Output.java
@@ -1,7 +1,20 @@
 package IO;
 
+import java.util.List;
+
 public class Output {
-    public void print(String word){
+    public void print(String word) {
         System.out.println(word);
+    }
+
+    public void displayWords(String phoneNumber, List<String> words) {
+        if (words.size() == 0) {
+            print("No matched words found for : " + phoneNumber);
+        } else {
+            print("Possible words for : " + phoneNumber);
+            for (String word : words) {
+                print(word);
+            }
+        }
     }
 }
--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,4 +1,5 @@
 import IO.Input;
+import IO.Output;
 import exceptions.NoArgumentException;
 import exceptions.NoSuchFileFoundException;
 import models.Dictionary;
@@ -7,21 +8,32 @@ import models.WordGenerator;
 import java.io.IOException;
 import java.util.List;
 
+import static helper.PhoneBookConstants.DATA_PATH;
+
 public class PhoneBook {
     static List<String> phoneNumbers;
     static Dictionary dictionary;
+    static WordGenerator wordGenerator;
+    static Output output;
 
     public static void main(String[] args) throws IOException, NoArgumentException, NoSuchFileFoundException {
         initialize(args);
-        WordGenerator wordGeneratorUtil = new WordGenerator(dictionary);
-//        wordGeneratorUtil.generateWord(phoneNumbers);
+        generateWords(phoneNumbers);
+    }
+
+    private static void generateWords(List<String> phoneNumbers) {
+        wordGenerator = new WordGenerator(dictionary);
+        output = new Output();
+        for (String phoneNumber : phoneNumbers) {
+            output.displayWords(phoneNumber, wordGenerator.generateWords(phoneNumber));
+        }
     }
 
     static void initialize(String[] args) throws IOException, NoSuchFileFoundException, NoArgumentException {
         Input input = new Input();
         if (args.length != 0) {
-            phoneNumbers = input.loadFile(args[0], "C:\\Users\\ganjali\\Documents\\Anjali\\Assignment-docstore\\PhoneBook\\src\\main\\java\\data");
-            dictionary = new Dictionary(input.loadFile(args[1], "C:\\Users\\ganjali\\Documents\\Anjali\\Assignment-docstore\\PhoneBook\\src\\main\\java\\data"));
+            phoneNumbers = input.loadFile(args[0], DATA_PATH);
+            dictionary = new Dictionary(input.loadFile(args[1], DATA_PATH));
         } else
             throw new NoArgumentException();
     }
